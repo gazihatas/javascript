cc2ef999cc4e48310be60dd45689d7299a09b789
Gazi Hatas <gazihatas71@gmail.com>
2022-01-16 00:25:56 +0300

    Operators

---
+++ ArinYazilimUygulamaliJS/Operators/app.js
@@ -0,0 +1,38 @@
+/* JAVASCRIPT OPERATORS */
+
+//Aritmetik Operatörler: + - * /
+/********************************
+ *  Operator precedence
+ 
+ */
+
+//let x = 4 + 3 * 5 - 6
+//console.log(x);
+
+
+//Asignment Operators: =
+let a = 5;
+a= 7;
+
+
+let x= 5;
+let y= "5";
+
+if(x === y){
+ alert("X ve Y birbirlerine eşittir")
+} else {
+    alert("X ve Y birbirlerine eşit değildir!")
+}
+
+
+//Modulus Operatoru %
+let year = 2017
+let kalan = year % 5
+console.log(kalan)
+document.write(kalan)
+
+
+//Increment ++
+var a = a+1
+
+//Decrement --

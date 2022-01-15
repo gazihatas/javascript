fa3586c3cf8b06de57598aa532fb1ff049442414
Gazi Hatas <gazihatas71@gmail.com>
2022-01-15 23:34:14 +0300

    Data Types

---
+++ ArinYazilimUygulamaliJS/DataTypes/app.js
@@ -0,0 +1,25 @@
+// PRIMITIVE DATA TYPES
+
+/*
+Number
+String
+Boolean
+Undefined
+Null
+
+*/
+
+let name = 'Gazi'
+console.log(typeof name)
+
+let age = 23
+console.log(typeof age)
+
+let isStudent = false;
+console.log(typeof isStudent)
+
+let sample;
+console.log(typeof sample)
+
+let sampleNull = null
+console.log(typeof sampleNull)

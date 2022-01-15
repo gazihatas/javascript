d23ce39e79e1f6846554c54f73ce6f6e15ecff6e
Gazi Hatas <gazihatas71@gmail.com>
2022-01-16 00:42:19 +0300

    If and Else

---
+++ ArinYazilimUygulamaliJS/IfElse/app.js
@@ -0,0 +1,21 @@
+/* IF / ELSE YAPISI */
+
+/*
+if(koşul){
+    bunu yap
+} else {
+    diğerni yap
+}
+*/
+
+let x = 5;
+if(x>4){
+    console.log("X 4 ten buyuktur")
+} else if( x < 4){
+    console.log("X 4 ten kucuktur")
+} else{
+    console.log("X 4 eşittir")
+}
+
+let  number = 5
+

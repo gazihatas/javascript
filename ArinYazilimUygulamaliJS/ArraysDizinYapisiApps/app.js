a5b6ca4c83aaf11d32646b4bd2728df6e8d8c044
Gazi Hatas <gazihatas71@gmail.com>
2022-01-16 06:20:15 +0300

    Arrays Dizin Yapısı Uygulama

---
+++ ArinYazilimUygulamaliJS/ArraysDizinYapisiApps/app.js
@@ -0,0 +1,10 @@
+
+let myArray = [4, 7, 5, 2], toplam = 0, carpim =1;
+
+for(let i = 0; i< myArray.length; i++) {
+
+    toplam += myArray[i]
+    carpim *= myArray[i]
+}
+
+console.log(`Toplam :  ${toplam}  Çarpım: ${carpim}`)

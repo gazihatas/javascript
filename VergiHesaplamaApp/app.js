45f7146f8be0ffa86d1a6d90b9dc70124dba8865
Gazi Hatas <gazihatas71@gmail.com>
2022-01-19 04:42:56 +0300

    Vergi Hesaplama

---
+++ VergiHesaplamaApp/app.js
@@ -0,0 +1,17 @@
+function vergiHesapla(){
+    let aracTipi;
+    let odenecekVergi;
+    let i;
+
+    for(i=0; i<document.getElementById("aracTipi").length; i++) {
+        console.log("run")
+        /*
+        if (document.getElementById("aracTipi").options[i].selected) {
+            aracTipi = document.getElementById("aracTipi").options[i].value;
+        } else if (document.getElementById("aracTipi").options[i].selected) {
+            aracTipi = document.getElementById("aracTipi").options[i].value;
+        }
+        console.log(aracTipi);
+        */
+    }
+}

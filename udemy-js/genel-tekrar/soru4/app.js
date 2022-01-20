48774efd26196d1ceff95c60c10459d98679a510
Gazi Hatas <gazihatas71@gmail.com>
2022-01-20 11:40:40 +0300

    Soru 4

---
+++ udemy-js/genel-tekrar/soru4/app.js
@@ -0,0 +1,19 @@
+/*
+    Kendi adınızı ekrana 5 kere yazdıran uygulamayı tüm döngülerle yazınız.
+*/
+
+for (let i = 0; i < 5; i++) {
+    console.log((i + 1) + " Gazi Hataş For")
+}
+
+let y = 0
+while (y > 5) {
+    console.log((y + 1) + "Gazi Hataş Do While")
+    y++;
+}
+
+let x = 0
+do {
+    console.log((y + 1) + "Gazi Hataş Do While")
+    x++;
+} while (x<5);
